42b086c8253b3ac08f6ca18846d2075838a06b42
Xscold <103187013+Xscold@users.noreply.github.com>
2022-10-01 15:16:29 +0800

    watchlist

--- raite2022-api/src/routes/user/user.js
+++ raite2022-api/src/routes/user/user.js
@@ -9,13 +9,8 @@ router.put("/update-user/:id", USER_CONTROLLER.UPDATE_USER)
 router.delete("/delete-user/:id", USER_CONTROLLER.DELETE_USER)
 router.post("/follow/:id/:userName", USER_CONTROLLER.FOLLOW_USER)
 router.post("/unfollow/:id/:userName", USER_CONTROLLER.UNFOLLOW_USER)
-<<<<<<< HEAD
-router.post("/add-coin-to-watchlist", USER_CONTROLLER.ADD_COIN_TO_WATCHLIST)
-router.get("/get-watchlist/:id", USER_CONTROLLER.GET_WATCH_LIST)
-=======
 router.post("/add-coin-to-watchlist/:userName", USER_CONTROLLER.ADD_COIN_TO_WATCHLIST)
-router.get("/get-watchlist", USER_CONTROLLER.GET_WATCH_LIST)
->>>>>>> 6f3fe6814c297ca5dd3f564a806f2c7e77f21de8
+router.get("/get-watchlist/:id", USER_CONTROLLER.GET_WATCH_LIST)
 
 
 module.exports = router
