492544f607270fea1640a3aafc59bf476e36317b
Xscold <103187013+Xscold@users.noreply.github.com>
2022-10-01 13:50:21 +0800

    post

--- raite2022-api/src/routes/post/post.js
+++ raite2022-api/src/routes/post/post.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const POST_CONTROLLER = require("../../controller/post/post")
 
 router.post("/create-post", POST_CONTROLLER.CREATE_POST)
-router.get("/get-all-post", POST_CONTROLLER.GET_ALL_POST)
+router.get("/get-all-post-from-user", POST_CONTROLLER.GET_ALL_POST_FROM_USER)
 router.put("/update-post", POST_CONTROLLER.UPDATE_POST)
 router.delete("/delete-post", POST_CONTROLLER.DELETE_POST)
-
+router.get("/get-all-post", POST_CONTROLLER.GET_ALL_POST)
 
 module.exports = router
